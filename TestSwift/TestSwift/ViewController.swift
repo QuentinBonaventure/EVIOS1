d9137f41262eebaf6568d2237ed83a5ed03e6ac4
Quentin <bonaventurequentin@gmail.com>
2022-07-07 11:10:38 +0200

    test 2 ok

--- TestSwift/TestSwift/ViewController.swift
+++ TestSwift/TestSwift/ViewController.swift
@@ -9,20 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var userImage :  UIImageView!
+    @IBOutlet var userImage:  UIImageView!
+    @IBOutlet var passwordField: UITextField!
+    @IBOutlet var passwordImage: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let recognizer =  UITapGestureRecognizer(target:self, action: #selector(changePasswordField))
+        recognizer.numberOfTapsRequired = 1
+        passwordImage.addGestureRecognizer(recognizer)
         
         
         // Do any additional setup after loading the view.
     }
+  
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         userImage.makeRounded()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            view.endEditing(true)
+        }
+    
+    @objc func changePasswordField(){
+        if(passwordField.isSecureTextEntry == true){
+            passwordField.isSecureTextEntry = false
+            passwordImage.image = UIImage(named: "eye_off_icon")
+        }else{
+            passwordField.isSecureTextEntry = true
+            passwordImage.image = UIImage(named: "eye_on_icon")
+        }
+        
+        print("test ok ")
+    }
 
+ 
 
 }
 
@@ -34,5 +58,7 @@ extension UIImageView{
         layer.cornerRadius = self.frame.height / 2
         clipsToBounds = true
     }
+    
+    
 }
 
